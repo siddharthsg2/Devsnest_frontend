a8ba5c76b0d5eb01310367715979c6cbd0ce45a0
Siddharth <siddharthguptasg2@gmail.com>
2021-06-25 17:57:46 +0530

    day 11

---
+++ Day11/script.js
@@ -0,0 +1,19 @@
+const quizDB = [
+    {
+        question: "1. Grand Central Terminal, Park Avenue, New York is the world's",
+        a: "largest railway station",
+        b: "highest railway station",
+        c: "longest railway station",
+        d: "None of the above",
+        ans: "ans1"
+    },
+    {
+        question: "2. Entomology is the science that studies",
+        a:	"Behavior of human beings",
+        b:	"Insects",
+        c:	"The origin and history of technical and scientific terms",
+        d:	"The formation of rocks",
+        ans: "ans2"
+    }
+]
+
