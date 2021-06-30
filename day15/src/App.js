f317b39b8e163002de112e890ed99fe64799295d
Siddharth <siddharthguptasg2@gmail.com>
2021-07-01 01:07:49 +0530

    day15

---
+++ day15/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {Component} from 'react'
+
+class App extends Component{
+  render() {
+    return (
+      <h1> Hello World </h1>
+    );
+  }
+}
+
+export default App;
