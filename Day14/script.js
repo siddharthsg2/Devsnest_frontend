d2cc433652757490138f9aeef208a114ed654d82
Siddharth <siddharthguptasg2@gmail.com>
2021-07-01 20:53:51 +0530

    day 14

---
+++ Day14/script.js
@@ -0,0 +1,28 @@
+const b1 = document.querySelector("#b1");
+const b2 = document.querySelector("#b2");
+const b3 = document.querySelector("#b3");
+const b4 = document.querySelector("#b4");
+
+b1.addEventListener("click", () => {
+  alert("Clicked!!");
+});
+
+b2.addEventListener("mouseover", () => {
+  alert("Hovered!!");
+});
+
+b3.addEventListener("mouseover", () => {
+  b3.innerHTML = "Magic!!";
+});
+
+b3.addEventListener("mouseout", () => {
+  b3.innerHTML = "Hover On Me";
+});
+
+b4.addEventListener("onmousedown", () => {
+  b4.innerHTML = "Magic!!";
+});
+
+b4.addEventListener("onmouseup", () => {
+  b4.innerHTML = "Click n Hold";
+});
