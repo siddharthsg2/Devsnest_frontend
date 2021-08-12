a6a6a4fbeaa978890a985fbee4bdccc9c099233d
Siddharth <siddharthguptasg2@gmail.com>
2021-08-12 20:07:32 +0530

    day28

--- day28/src/App.js
+++ day28/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState} from 'react';
 
 function App() {
+  const updatePlaceData=()=>{
+    fetch(`https://api.weatherapi.com/v1/current.json?key=a1089418425d486c9a0185247210908&q=${place}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setPlaceData(data)
+    })
+  }
+  const [place,setPlace]= useState("");
+  const [placeData ,setPlaceData]= useState({});
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather Card web-app</h1>
+      
+        <input type="text" 
+        value={place}
+        placeholder="delhi"
+        onChange={(e)=>{
+          setPlace(e.target.value)
+          }} 
+        />
+        <button onClick={updatePlaceData}>search</button>
+        <div className="card">
+          {placeData.location ? 
+          (
+          <>
+            <img src={placeData.current.condition.icon} alt="img" />
+            <div className="temp">{placeData.current.temp_c}°</div>
+            <div className="cityName">{placeData.location.name} </div>
+            <div className="container">
+              <div className="row">
+                <div className="col">Wind Now <div className="data">{placeData.current.wind_kph} <span>Km/hr</span> </div></div>
+                
+                <div className="col">Humidity <div className="data">{placeData.current.humidity} <span>%</span></div></div>
+                
+                <div className="col">Precipitation <div className="data">{placeData.current.precip_in} <span>%</span></div></div>
+
+              </div>
+            </div>
+
+
+         </>
+         )
+          :(<h5>not found</h5>)}
+        </div>
     </div>
   );
 }
